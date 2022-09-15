ec02d28490f1d3c147c74909aa54bfa30ed838ac
Tiago de Carvalho <108219612+tiagoCB41@users.noreply.github.com>
2022-09-15 09:23:05 -0300

    Update main.java

--- JAVA/thread/intro/main.java
+++ JAVA/thread/intro/main.java
@@ -2,17 +2,22 @@ package com.thread.intro;
 
 public class main {
     public static void main(String[] args){
-
+        
+        //ENTRE EXTENDER A CLASSE THREAD OU IMPLEMENTAR A CLASSE RUNNABLE
+        //A MELHOR OPÇÃO VAI DEPENDER DA NECESSIDADE
+        //LEVANDO EM CONTA QUE UMA CLASSE SÓ PODE EXTENDER UMA CLASSE MÃE
+        //CASO SEJA NECESSÁRIO EXTENDER ALGUMA OUTRA CLASSE, SE TORNA VIÁVEL
+        //TER QUE IMPLEMENTAR A INTERFACE RUNNABLE
         MinhaThread thread = new MinhaThread("thread #1", 500);
 
         MinhaThread thread2 = new MinhaThread("thread #2",2000);
 
-        //SE CHAMAR COM O METODO THREAD, S�O EXECUTADOS SIMULTANEAMENTE
+        //SE CHAMAR COM O METODO THREAD, SÃO EXECUTADOS SIMULTANEAMENTE
         System.out.println("**metodo thread");
         thread.start();
         thread2.start();
 
-        //CASO CONTR�RIO (metodo run) SER� EXECUTADO UM DE CADA VEZ
+        //CASO CONTRÁRIO (metodo run) SERÁ EXECUTADO UM DE CADA VEZ
 
 
 }}
